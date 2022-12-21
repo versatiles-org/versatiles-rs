30e441e637b9397bb8d8d5c6031f54f12dbb18e9
Michael Kreil <github@michael-kreil.de>
2022-12-21 22:12:07 +0100

    fix clap

--- src/container/mod.rs
+++ src/container/mod.rs
@@ -5,18 +5,18 @@ mod mbtiles;
 pub use abstract_classes::*;
 use std::path::PathBuf;
 
-use crate::Cli;
+use crate::Convert;
 
 pub struct Tools;
 impl Tools {
-	pub fn convert(filename_in: &PathBuf, filename_out: &PathBuf, cli: &Cli) -> std::io::Result<()> {
-		let container_in = Tools::new_reader(filename_in, cli)?;
-		let mut converter = Tools::new_converter(filename_out, cli)?;
+	pub fn convert(command: &Convert) -> std::io::Result<()> {
+		let container_in = Tools::new_reader(&command.input_file, command)?;
+		let mut converter = Tools::new_converter(&command.output_file, command)?;
 		converter.convert_from(container_in)?;
 
 		return Ok(());
 	}
-	pub fn new_reader(filename: &PathBuf, cli: &Cli) -> std::io::Result<Box<dyn Reader>> {
+	pub fn new_reader(filename: &PathBuf, command: &Convert) -> std::io::Result<Box<dyn Reader>> {
 		let extension = filename.extension().unwrap().to_str();
 		let mut container = match extension {
 			Some("mbtiles") => mbtiles::Reader::load(filename)?,
@@ -24,15 +24,15 @@ impl Tools {
 			_ => panic!("extension '{:?}' unknown", extension),
 		};
 
-		if cli.min_zoom.is_some() {
-			let zoom: u64 = cli.min_zoom.unwrap();
+		if command.min_zoom.is_some() {
+			let zoom: u64 = command.min_zoom.unwrap();
 			if container.get_minimum_zoom() < zoom {
 				container.set_minimum_zoom(zoom)
 			}
 		}
 
-		if cli.max_zoom.is_some() {
-			let zoom: u64 = cli.max_zoom.unwrap();
+		if command.max_zoom.is_some() {
+			let zoom: u64 = command.max_zoom.unwrap();
 			if container.get_maximum_zoom() > zoom {
 				container.set_maximum_zoom(zoom)
 			}
@@ -40,7 +40,10 @@ impl Tools {
 
 		return Ok(container);
 	}
-	pub fn new_converter(filename: &PathBuf, cli: &Cli) -> std::io::Result<Box<dyn Converter>> {
+	pub fn new_converter(
+		filename: &PathBuf,
+		command: &Convert,
+	) -> std::io::Result<Box<dyn Converter>> {
 		let extension = filename.extension().unwrap().to_str();
 		let mut converter = match extension {
 			Some("mbtiles") => mbtiles::Converter::new(filename).unwrap(),
@@ -48,8 +51,8 @@ impl Tools {
 			_ => panic!("extension '{:?}' unknown", extension),
 		};
 
-		if cli.precompress.is_some() {
-			converter.set_precompression(cli.precompress.as_ref().unwrap());
+		if command.precompress.is_some() {
+			converter.set_precompression(command.precompress.as_ref().unwrap());
 		}
 
 		return Ok(converter);
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,24 @@ use crate::container::Tools;
 #[command(author, version, about, long_about = None)]
 #[command(propagate_version = true)]
 pub struct Cli {
+	#[command(subcommand)]
+	command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+	/// convert between different tile containers
+	Convert(Convert),
+}
+
+#[derive(Args)]
+pub struct Convert {
+	/// (e.g. *.mbtiles, *.cloudtiles, *.tar)
+	input_file: PathBuf,
+
+	/// (e.g. *.mbtiles, *.cloudtiles, *.tar)
+	output_file: PathBuf,
+
 	/// minimal zoom level
 	#[arg(long, value_name = "int")]
 	min_zoom: Option<u64>,
@@ -20,26 +38,6 @@ pub struct Cli {
 	/// precompress tiles
 	#[arg(long, value_enum)]
 	precompress: Option<container::TileCompression>,
-
-	#[command(subcommand)]
-	command: Commands,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-	/// convert between different tile containers
-	Convert {
-		/// (e.g. *.mbtiles, *.cloudtiles, *.tar)
-		input_file: PathBuf,
-
-		/// (e.g. *.mbtiles, *.cloudtiles, *.tar)
-		output_file: PathBuf,
-	},
-}
-
-#[derive(Args)]
-struct Convert {
-	name: Option<String>,
 }
 
 fn main() -> std::io::Result<()> {
@@ -47,12 +45,12 @@ fn main() -> std::io::Result<()> {
 
 	let command = &cli.command;
 	match command {
-		Commands::Convert {
-			input_file,
-			output_file,
-		} => {
-			println!("convert from {:?} to {:?}", input_file, output_file);
-			Tools::convert(input_file, output_file, &cli)?;
+		Commands::Convert(arguments) => {
+			println!(
+				"convert from {:?} to {:?}",
+				arguments.input_file, arguments.output_file
+			);
+			Tools::convert(&arguments)?;
 			return Ok(());
 		}
 	}
