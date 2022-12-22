5a8c71ae2c8726dc3e92abfeae847c7491ff353d
Michael Kreil <github@michael-kreil.de>
2022-12-22 20:01:51 +0100

    move default implementation

--- src/opencloudtiles/abstract_classes/reader.rs
+++ src/opencloudtiles/abstract_classes/reader.rs
@@ -7,15 +7,34 @@ use super::{TileCompression, TileFormat};
 pub trait TileReader {
 	fn load(filename: &PathBuf) -> std::io::Result<Box<dyn TileReader>>
 	where
-		Self: Sized;
-	fn get_tile_format(&self) -> TileFormat;
-	fn get_tile_compression(&self) -> TileCompression;
-	fn get_meta(&self) -> &[u8];
-	fn get_minimum_zoom(&self) -> u64;
-	fn get_maximum_zoom(&self) -> u64;
-	fn get_level_bbox(&self, level: u64) -> (u64, u64, u64, u64);
-	fn get_tile_uncompressed(&self, level: u64, col: u64, row: u64) -> Option<Vec<u8>>;
-	fn get_tile_raw(&self, level: u64, col: u64, row: u64) -> Option<Vec<u8>>;
+		Self: Sized,
+	{
+		panic!();
+	}
+	fn get_tile_format(&self) -> TileFormat {
+		panic!();
+	}
+	fn get_tile_compression(&self) -> TileCompression {
+		panic!();
+	}
+	fn get_meta(&self) -> &[u8] {
+		panic!();
+	}
+	fn get_minimum_zoom(&self) -> u64 {
+		panic!();
+	}
+	fn get_maximum_zoom(&self) -> u64 {
+		panic!();
+	}
+	fn get_level_bbox(&self, level: u64) -> (u64, u64, u64, u64) {
+		panic!();
+	}
+	fn get_tile_uncompressed(&self, level: u64, col: u64, row: u64) -> Option<Vec<u8>> {
+		panic!();
+	}
+	fn get_tile_raw(&self, level: u64, col: u64, row: u64) -> Option<Vec<u8>> {
+		panic!();
+	}
 }
 
 pub struct TileReaderWrapper<'a> {
--- src/opencloudtiles/cloudtiles/reader.rs
+++ src/opencloudtiles/cloudtiles/reader.rs
@@ -1,36 +1,4 @@
 use crate::opencloudtiles;
 
 pub struct TileReader;
-impl opencloudtiles::TileReader for TileReader {
-	#![allow(unused_variables)]
-	fn get_level_bbox(&self, level: u64) -> (u64, u64, u64, u64) {
-		panic!();
-	}
-	fn get_meta(&self) -> &[u8] {
-		panic!();
-	}
-	fn get_maximum_zoom(&self) -> u64 {
-		panic!();
-	}
-	fn get_minimum_zoom(&self) -> u64 {
-		panic!();
-	}
-	fn get_tile_compression(&self) -> opencloudtiles::TileCompression {
-		panic!();
-	}
-	fn get_tile_format(&self) -> opencloudtiles::TileFormat {
-		panic!();
-	}
-	fn get_tile_raw(&self, level: u64, col: u64, row: u64) -> Option<Vec<u8>> {
-		panic!();
-	}
-	fn get_tile_uncompressed(&self, level: u64, col: u64, row: u64) -> Option<Vec<u8>> {
-		panic!();
-	}
-	fn load(filename: &std::path::PathBuf) -> std::io::Result<Box<dyn opencloudtiles::TileReader>>
-	where
-		Self: Sized,
-	{
-		panic!();
-	}
-}
+impl opencloudtiles::TileReader for TileReader {}
