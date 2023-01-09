05ba5f5029687f5dc6e178a393933d3ed0b69adf
Michael Kreil <github@michael-kreil.de>
2023-01-09 09:11:38 +0100

    move check script

---
+++ bin/check.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "check cargo fmt"
+result=$(cargo fmt -- --check 2>&1)
+if [ $? -ne 0 ]; then
+   echo "$result"
+   echo "ERROR DURING: cargo fmt"
+   exit 1
+fi
+
+echo "check cargo clippy "
+result=$(cargo clippy -- -D warnings 2>&1)
+if [ $? -ne 0 ]; then
+   echo "$result"
+   echo "ERROR DURING: cargo clippy"
+   exit 1
+fi
+
+echo "check cargo test"
+result=$(cargo test 2>&1)
+if [ $? -ne 0 ]; then
+   echo "$result"
+   echo "ERROR DURING: cargo test"
+   exit 1
+fi
+
+exit 0
