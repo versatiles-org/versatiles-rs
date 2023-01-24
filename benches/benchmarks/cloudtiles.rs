ec9df1567e92c1267461d3ebb5ddd9ada16940e8
Michael Kreil <github@michael-kreil.de>
2023-01-24 19:51:48 +0100

    move test data

--- benches/benchmarks/cloudtiles.rs
+++ benches/benchmarks/cloudtiles.rs
@@ -8,14 +8,15 @@ use rand::seq::SliceRandom;
 
 fn cloudtiles_read(c: &mut Criterion) {
 	set_max_level(LevelFilter::Warn);
-	let reader = TileReader::new("benches/ressources/2023-01-eu-de-be.cloudtiles");
-	let coords: Vec<TileCoord3> = reader
-		.get_parameters()
-		.get_bbox_pyramide()
-		.iter_tile_indexes()
-		.collect();
 
 	c.bench_function("get_tile_data", |b| {
+		let reader = TileReader::new("benches/resources/berlin.cloudtiles");
+		let coords: Vec<TileCoord3> = reader
+			.get_parameters()
+			.get_bbox_pyramide()
+			.iter_tile_indexes()
+			.collect();
+
 		b.iter(|| {
 			let coord = coords.choose(&mut rand::thread_rng()).unwrap();
 			black_box(reader.get_tile_data(coord));
--- benches/benchmarks/mbtiles.rs
+++ benches/benchmarks/mbtiles.rs
@@ -5,23 +5,11 @@ use opencloudtiles::{
 	helper::*,
 };
 
-/*
-fn mbtiles_new(c: &mut Criterion) {
-	set_max_level(LevelFilter::Warn);
-	c.bench_function("TileReader::new mbtiles", |b| {
-		b.iter(|| {
-			TileReader::new("benches/ressources/2023-01-eu-de-be.mbtiles");
-		});
-	});
-}
-criterion_group!(benches, mbtiles_new);
-*/
-
 fn mbtiles_read_vec(c: &mut Criterion) {
 	set_max_level(LevelFilter::Warn);
-	let reader = TileReader::new("benches/ressources/2023-01-eu-de-be.mbtiles");
 
 	c.bench_function("get_bbox_tile_vec", |b| {
+		let reader = TileReader::new("benches/resources/berlin.mbtiles");
 		b.iter(|| {
 			black_box(reader.get_bbox_tile_vec(14, &TileBBox::new(8787, 5361, 8818, 5387)));
 		})
