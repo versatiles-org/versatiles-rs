e10802cec104f6cf9652b9f77225c68b782a3da7
Michael Kreil <github@michael-kreil.de>
2023-03-17 14:36:53 +0100

    clippy fixes

--- src/lib/container/versatiles/types/versatiles_src.rs
+++ src/lib/container/versatiles/types/versatiles_src.rs
@@ -26,11 +26,11 @@ pub trait VersaTilesSrcTrait {
 pub fn new_versatiles_src(source: &str) -> Box<dyn VersaTilesSrcTrait> {
 	let start = source.split_terminator(':').next();
 
-	return match start {
+	match start {
 		Some("gs") => Box::new(VersaTilesSrcObjectStore::new(source).unwrap()),
 		Some("http" | "https") => Box::new(VersaTilesSrcHttp::new(source).unwrap()),
 		_ => Box::new(VersaTilesSrcFile::new(source).unwrap()),
-	};
+	}
 }
 
 struct VersaTilesSrcFile {
