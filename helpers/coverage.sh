3a248390a5a9e9886e6c8b6a2d68d692a9646642
Michael Kreil <github@michael-kreil.de>
2023-04-03 17:24:40 +0200

    fix coverage helper

--- helpers/coverage.sh
+++ helpers/coverage.sh
@@ -2,16 +2,4 @@
 cd "$(dirname "$0")"
 cd ..
 
-# prepare
-rm -rf coverage
-mkdir coverage
-
-# generate test coverage
-mkdir coverage/profraw
-CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE='./coverage/profraw/cargo-test-%p-%m.profraw' cargo test --workspace
-
-# generate html
-grcov coverage/profraw/. --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o ./coverage/html
-
-# generate lcov
-grcov coverage/profraw/. --binary-path ./target/debug/deps/ -s . -t lcov --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o ./coverage/tests.lcov
+cargo llvm-cov test --workspace --tests --lcov --output-path ./lcov.info
