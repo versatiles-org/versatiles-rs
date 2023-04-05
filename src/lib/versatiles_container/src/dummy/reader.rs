3a6d2bcb464de3343945c7ff46c0dbe73ee18423
Michael Kreil <github@michael-kreil.de>
2023-04-05 16:33:06 +0200

    improve dummy

--- src/lib/versatiles_container/src/dummy/reader.rs
+++ src/lib/versatiles_container/src/dummy/reader.rs
@@ -14,10 +14,10 @@ impl TileReaderTrait for TileReader {
 		}))
 	}
 	fn get_container_name(&self) -> &str {
-		"dummy"
+		"dummy container"
 	}
 	fn get_name(&self) -> &str {
-		"dummy.bin"
+		"dummy name"
 	}
 	fn get_parameters(&self) -> &TileReaderParameters {
 		&self.parameters
@@ -26,17 +26,17 @@ impl TileReaderTrait for TileReader {
 		&mut self.parameters
 	}
 	async fn get_meta(&self) -> Blob {
-		Blob::empty()
+		Blob::from("dummy meta data")
 	}
 	async fn get_tile_data(&self, _coord: &TileCoord3) -> Option<Blob> {
-		Some(Blob::from("dummydata"))
+		Some(Blob::from("dummy tile data"))
 	}
 	async fn deep_verify(&self) {}
 }
 
 impl std::fmt::Debug for TileReader {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		f.debug_struct("TileReader:MBTiles")
+		f.debug_struct("TileReader:Dummy")
 			.field("parameters", &self.get_parameters())
 			.finish()
 	}
@@ -51,14 +51,14 @@ mod tests {
 	#[test]
 	fn test1() {
 		let mut reader = block_on(dummy::TileReader::new("filename.txt")).unwrap();
-		assert_eq!(reader.get_container_name(), "dummy");
-		assert_eq!(reader.get_name(), "dummy.bin");
+		assert_eq!(reader.get_container_name(), "dummy container");
+		assert_eq!(reader.get_name(), "dummy name");
 		assert_eq!(reader.get_parameters(), &TileReaderParameters::new_dummy());
 		assert_eq!(reader.get_parameters_mut(), &mut TileReaderParameters::new_dummy());
-		assert_eq!(block_on(reader.get_meta()), Blob::empty());
+		assert_eq!(block_on(reader.get_meta()), Blob::from("dummy meta data"));
 		assert_eq!(
 			block_on(reader.get_tile_data(&TileCoord3::new_empty())).unwrap(),
-			Blob::from("dummydata")
+			Blob::from("dummy tile data")
 		);
 	}
 
