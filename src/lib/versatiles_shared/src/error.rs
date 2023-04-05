f84676dc1f3cf13b439b89eef34e8e9a6eba43ea
Michael Kreil <github@michael-kreil.de>
2023-04-05 20:29:02 +0200

    tests for error

--- src/lib/versatiles_shared/src/error.rs
+++ src/lib/versatiles_shared/src/error.rs
@@ -1,9 +1,3 @@
-// use http::header::InvalidHeaderValue as http_header_InvalidHeaderValue;
-// use reqwest::Error as reqwest_Error;
-// use std::fmt::Error as std_fmt_Error;
-// use std::io::Error as std_io_Error;
-// use url::ParseError as url_ParseError;
-
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 #[derive(Debug, Clone)]
@@ -25,18 +19,15 @@ impl std::fmt::Display for Error {
 
 impl std::error::Error for Error {}
 
-/*
-impl std::fmt::Debug for Error {
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		f.debug_struct("Error").finish()
+#[cfg(test)]
+mod tests {
+	use super::Error;
+
+	#[test]
+	fn test() {
+		let err = Error::new(String::from("hi"));
+		let err = err.clone();
+		format!("{}", err);
+		format!("{:?}", err);
 	}
 }
-
-impl std::fmt::Display for Error {
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		write!(f, "{}", self.msg)
-	}
-}
-
-impl std::error::Error for Error {}
- */
