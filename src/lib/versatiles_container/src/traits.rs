805fd6fff50e994c88a9a7511d2d861604961182
Michael Kreil <github@michael-kreil.de>
2023-04-07 10:25:10 +0200

    add tests

--- src/lib/versatiles_container/src/traits.rs
+++ src/lib/versatiles_container/src/traits.rs
@@ -64,3 +64,93 @@ pub trait TileReaderTrait: Debug + Send + Sync {
 
 	async fn deep_verify(&self);
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use async_trait::async_trait;
+
+	#[derive(Debug)]
+	struct TestReader {
+		name: String,
+		parameters: TileReaderParameters,
+	}
+
+	#[async_trait]
+	impl TileReaderTrait for TestReader {
+		async fn new(path: &str) -> Result<TileReaderBox> {
+			let parameters = TileReaderParameters::new_dummy();
+			let reader = TestReader {
+				name: path.to_owned(),
+				parameters,
+			};
+			Ok(Box::new(reader))
+		}
+
+		fn get_name(&self) -> &str {
+			&self.name
+		}
+
+		fn get_parameters(&self) -> &TileReaderParameters {
+			&self.parameters
+		}
+
+		fn get_parameters_mut(&mut self) -> &mut TileReaderParameters {
+			&mut self.parameters
+		}
+
+		async fn get_meta(&self) -> Blob {
+			Blob::from("test metadata")
+		}
+
+		fn get_container_name(&self) -> &str {
+			"test container name"
+		}
+
+		async fn deep_verify(&self) {}
+
+		async fn get_tile_data(&self, _coord: &TileCoord3) -> Option<Blob> {
+			Some(Blob::from("test tile data"))
+		}
+	}
+
+	struct TestConverter {}
+
+	#[async_trait]
+	impl TileConverterTrait for TestConverter {
+		fn new(_filename: &Path, _config: TileConverterConfig) -> TileConverterBox
+		where
+			Self: Sized,
+		{
+			let converter = TestConverter {};
+			Box::new(converter)
+		}
+
+		async fn convert_from(&mut self, _reader: &mut TileReaderBox) {}
+	}
+
+	#[tokio::test]
+	async fn test_reader() {
+		let reader = TestReader::new("test_path").await.unwrap();
+
+		// Test getting name
+		assert_eq!(reader.get_name(), "test_path");
+
+		// Test getting tile precompression and format
+		assert_eq!(reader.get_tile_precompression(), &Precompression::Uncompressed);
+		assert_eq!(reader.get_tile_format(), &TileFormat::PBF);
+
+		// Test getting container name
+		assert_eq!(reader.get_container_name(), "test container name");
+
+		// Test getting metadata
+		assert_eq!(reader.get_meta().await.to_string(), "test metadata");
+
+		// Test getting tile data
+		let coord = TileCoord3::new(0, 0, 0);
+		assert_eq!(
+			reader.get_tile_data(&coord).await.unwrap().to_string(),
+			"test tile data"
+		);
+	}
+}
