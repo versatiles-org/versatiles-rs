c2f9301b29ad877624c341c8cc428b12d84242c9
Michael Kreil <github@michael-kreil.de>
2023-05-01 22:28:06 +0200

    test data reader file

--- src/containers/versatiles/abstract_readers/data_reader_file.rs
+++ src/containers/versatiles/abstract_readers/data_reader_file.rs
@@ -48,3 +48,77 @@ impl DataReaderTrait for DataReaderFile {
 		&self.name
 	}
 }
+#[cfg(test)]
+mod tests {
+	use super::{DataReaderFile, DataReaderTrait};
+	use crate::{containers::versatiles::types::ByteRange, shared::Result};
+	use assert_fs::NamedTempFile;
+	use std::{fs::File, io::Write};
+
+	// Test the 'new' method for valid and invalid files
+	#[tokio::test]
+	async fn test_new() -> Result<()> {
+		let temp_file_path = NamedTempFile::new("testfile.txt")?;
+		let invalid_path = NamedTempFile::new("nonexistent.txt")?;
+
+		// Create a temporary file
+		{
+			let mut temp_file = File::create(&temp_file_path)?;
+			temp_file.write_all(b"Hello, world!")?;
+		}
+
+		// Test with a valid file path
+		let data_reader_file = DataReaderFile::new(temp_file_path.to_str().unwrap()).await;
+		assert!(data_reader_file.is_ok());
+
+		// Test with an invalid file path
+		let data_reader_file = DataReaderFile::new(invalid_path.to_str().unwrap()).await;
+		assert!(data_reader_file.is_err());
+
+		Ok(())
+	}
+
+	// Test the 'read_range' method
+	#[tokio::test]
+	async fn test_read_range() -> Result<()> {
+		let temp_file_path = NamedTempFile::new("testfile.txt")?;
+
+		// Create a temporary file
+		{
+			let mut temp_file = File::create(&temp_file_path)?;
+			temp_file.write_all(b"Hello, world!")?;
+		}
+
+		let mut data_reader_file = DataReaderFile::new(temp_file_path.to_str().unwrap()).await?;
+
+		// Define a range to read
+		let range = ByteRange { offset: 4, length: 6 };
+
+		// Read the specified range from the file
+		let blob = data_reader_file.read_range(&range).await?;
+
+		// Check if the read range matches the expected text
+		assert_eq!(blob.as_str(), "o, wor");
+
+		Ok(())
+	}
+
+	// Test the 'get_name' method
+	#[tokio::test]
+	async fn test_get_name() -> Result<()> {
+		let temp_file_path = NamedTempFile::new("testfile.txt")?;
+
+		// Create a temporary file
+		{
+			let mut temp_file = File::create(&temp_file_path)?;
+			temp_file.write_all(b"Hello, world!")?;
+		}
+
+		let data_reader_file = DataReaderFile::new(temp_file_path.to_str().unwrap()).await?;
+
+		// Check if the name matches the original file path
+		assert_eq!(data_reader_file.get_name(), temp_file_path.to_str().unwrap());
+
+		Ok(())
+	}
+}
