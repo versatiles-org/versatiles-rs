2e8bd3f70954d6060706ec8ac5512a67cc5a693c
Michael Kreil <github@michael-kreil.de>
2023-05-30 13:04:31 +0200

    remove bench

--- benches_fix-me-please/benchmarks/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod mbtiles;
-pub mod server;
-pub mod versatiles;
--- benches_fix-me-please/main.rs
+++
@@ -1,9 +0,0 @@
-mod benchmarks;
-
-use criterion::criterion_main;
-
-criterion_main! {
-	benchmarks::mbtiles::mbtiles,
-	benchmarks::versatiles::versatiles,
-	benchmarks::server::server,
-}
