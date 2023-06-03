dcb8a99847b401b3678d088f470f1e5f65e0c589
Michael Kreil <github@michael-kreil.de>
2023-06-03 16:27:43 +0200

    improve bench

--- benches/mbtiles.rs
+++ benches/mbtiles.rs
@@ -1,17 +1,19 @@
 use criterion::{black_box, criterion_group, Criterion};
-use futures::executor::block_on;
+use futures::{executor::block_on, StreamExt};
 use log::{set_max_level, LevelFilter};
 use versatiles::{containers::get_reader, shared::TileBBox};
 
 fn mbtiles_read_vec(c: &mut Criterion) {
 	set_max_level(LevelFilter::Warn);
 
-	c.bench_function("get_bbox_tile_vec", |b| {
-		let mut reader = block_on(get_reader("benches/ressources/berlin.mbtiles")).unwrap();
+	c.bench_function("get_bbox_tile_stream", |b| {
+		let mut reader = block_on(get_reader("testdata/berlin.mbtiles")).unwrap();
 		b.iter(|| {
-			black_box(block_on(
-				reader.get_bbox_tile_stream(&TileBBox::new(14, 8787, 5361, 8818, 5387)),
-			));
+			block_on(async {
+				let bbox = TileBBox::new(14, 8787, 5361, 8818, 5387);
+				let stream = reader.get_bbox_tile_stream(&bbox).await;
+				let _count = stream.count().await;
+			});
 		})
 	});
 }
