272aa72fd5d1b6839b98775b72df43acf0a88c39
Michael Kreil <github@michael-kreil.de>
2023-09-02 16:33:30 +0200

    check unused dependencies

---
+++ helpers/unused_dependencies.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cargo +nightly udeps --quiet --lib
+cargo +nightly udeps --quiet --lib --no-default-features
+cargo +nightly udeps --quiet --bin versatiles
+cargo +nightly udeps --quiet --bin versatiles --no-default-features
+cargo +nightly udeps --quiet --bin versatiles --no-default-features --features mbtiles
+cargo +nightly udeps --quiet --bin versatiles --no-default-features --features server
+cargo +nightly udeps --quiet --bin versatiles --no-default-features --features mbtiles server
