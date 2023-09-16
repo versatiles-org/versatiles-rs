76b9470030424688b93110e9219bed24539d2588
Michael Kreil <github@michael-kreil.de>
2023-09-16 20:54:37 +0200

    upgrade dependencies

--- helpers/test_features.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo -e "\033[1;30mtest bin\033[0m"
-cargo test --quiet --bin versatiles --all-features
-
-echo -e "\033[1;30mtest lib\033[0m"
-cargo test --quiet --lib
-
-exit 0
