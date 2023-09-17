c2c370e913ab5cbf6c6707dab1a9abb13ec5ea2e
Michael Kreil <github@michael-kreil.de>
2023-09-17 15:48:13 +0200

    improve release, #53 + #55

---
+++ helpers/workflow-create_release.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")/.."
+
+# get latest tags
+curl -s https://api.github.com/repos/versatiles-org/versatiles-rs/tags >tags.json
+
+# get new tag
+NEW_TAG=$(jq -r "nth(0; .[] | .name | select(startswith('v')))" tags.json)
+# get old tag
+OLD_TAG=$(jq -r "nth(1; .[] | .name | select(startswith('v')))" tags.json)
+# get old SHA
+OLD_SHA=$(jq -r ".[] | select(.name == \"$OLD_TAG\") | .commit.sha" tags.json)
+
+# get version via cargo.toml
+VERSION=$(cat Cargo.toml | sed -n "s/^version *= *\"\(.*\)\"/version=v\1/p" | tr -d '\n')
+
+# compare versions
+if [ "$NEW_TAG" != "$VERSION" ]; then
+	echo "Current cargo version ($VERSION) is not latest tag ($NEW_TAG)"
+	exit 1
+fi
+
+echo "# new release: $NEW_TAG" >notes.txt
+
+curl -s "https://api.github.com/repos/versatiles-org/versatiles-rs/commits?per_page=100" | jq -r ".[] | if .sha == \"$OLD_SHA\" then halt else \"- \" + .commit.message end" >>notes.txt
+
+# Try to create release
+gh release view "$NEW_TAG" || gh release create "$NEW_TAG" --title "$NEW_TAG" -F notes.txt --draft --prerelease
+
+# return Version
+echo "tag=$NEW_TAG" >>"$GITHUB_OUTPUT"
---
+++ helpers/workflow-pack_and_upload.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+FOLDER="$1"
+FILENAME="versatiles-$2"
+TAG=$3
+
+cd "$FOLDER"
+tar -cf "$FILENAME.tar" "versatiles"
+gzip -9 "$FILENAME.tar"
+sha256sum "$FILENAME.tar.gz" | sed 's/ .*//' >"$FILENAME.tar.gz.sha256"
+md5sum "$FILENAME.tar.gz" | sed 's/ .*//' >"$FILENAME.tar.gz.md5"
+gh release upload "$TAG" $FILENAME.tar.gz* --clobber
