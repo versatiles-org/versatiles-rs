ea1bd003043d030f11395381a07dcf7518e66121
Michael Kreil <github@michael-kreil.de>
2023-09-17 18:12:07 +0200

    fix: hash calculation during release on mac

--- helpers/workflow-pack_and_upload.sh
+++ helpers/workflow-pack_and_upload.sh
@@ -9,6 +9,18 @@ TAG=$3
 cd "$FOLDER"
 tar -cf "$FILENAME.tar" "versatiles"
 gzip -9 "$FILENAME.tar"
-sha256sum "$FILENAME.tar.gz" | sed 's/ .*//' >"$FILENAME.tar.gz.sha256"
-md5sum "$FILENAME.tar.gz" | sed 's/ .*//' >"$FILENAME.tar.gz.md5"
+
+case $(uname -s) in
+   Linux*)
+      sha256sum "$FILENAME.tar.gz" | sed 's/ .*//' >"$FILENAME.tar.gz.sha256"
+      md5sum "$FILENAME.tar.gz" | sed 's/ .*//' >"$FILENAME.tar.gz.md5"
+      ;;
+   Darwin*)
+      shasum -a 256 "$FILENAME.tar.gz" | sed 's/ .*//' >"$FILENAME.tar.gz.sha256"
+      md5 -q "$FILENAME.tar.gz"  >"$FILENAME.tar.gz.md5"
+      ;;
+   *)
+      echo "Unknown OS: $(uname -s)"
+      ;;
+esac
 gh release upload "$TAG" $FILENAME.tar.gz* --clobber
