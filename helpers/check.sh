38a203cee12ea971828256c0b2f000b8b9adb036
Michael Kreil <github@michael-kreil.de>
2023-09-19 00:23:02 +0200

    fix: cargo clippy --all-features

--- helpers/check.sh
+++ helpers/check.sh
@@ -5,32 +5,28 @@ cd "$(dirname "$0")/.."
 echo "check cargo fmt"
 result=$(cargo fmt -- --check 2>&1)
 if [ $? -ne 0 ]; then
-   echo "$result"
-   echo "ERROR DURING: cargo fmt"
+   echo -e "$result\nERROR DURING: cargo fmt"
    exit 1
 fi
 
 echo "check cargo clippy "
-result=$(cargo clippy -- -D warnings 2>&1)
+result=$(cargo clippy --bin versatiles --all-features -- -D warnings 2>&1)
 if [ $? -ne 0 ]; then
-   echo "$result"
-   echo "ERROR DURING: cargo clippy"
+   echo -e "$result\nERROR DURING: cargo clippy"
    exit 1
 fi
 
 echo "check cargo test lib"
 result=$(cargo test --lib 2>&1)
 if [ $? -ne 0 ]; then
-   echo "$result"
-   echo "ERROR DURING: cargo test lib"
+   echo -e "$result\nERROR DURING: cargo test lib"
    exit 1
 fi
 
 echo "check cargo test bin"
 result=$(cargo test --bin versatiles --all-features 2>&1)
 if [ $? -ne 0 ]; then
-   echo "$result"
-   echo "ERROR DURING: cargo test bin"
+   echo -e "$result\nERROR DURING: cargo test bin"
    exit 1
 fi
 
