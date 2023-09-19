014b84fced719e9af0d4651843a556a3ad7b49be
Michael Kreil <github@michael-kreil.de>
2023-09-19 10:24:17 +0200

    fix: helpers

--- helpers/release.sh
+++ helpers/release.sh
@@ -8,24 +8,32 @@ RED="\033[1;31m"
 GRE="\033[1;32m"
 END="\033[0m"
 
-cargo check
+cargo check --workspace
 
 # check if nothing to commit
-if [ "$(git status --porcelain)" ]; then
+if [ -n "$(git status --porcelain)" ]; then
 	echo "❗️ Please commit all uncommitted changes!"
 	exit 1
 fi
 
 # get versions
 old_tag=$(curl -s "https://api.github.com/repos/versatiles-org/versatiles-rs/tags" | jq -r 'first(.[] | .name | select(startswith("v")))')
-new_tag=v$(cat Cargo.toml | sed -ne 's/^version[ ="]*\([0-9\.]*\).*$/\1/p')
+tag_bin=v$(cat versatiles/Cargo.toml | sed -ne 's/^version[ ="]*\([0-9\.]*\).*$/\1/p')
+tag_lib=v$(cat versatiles_lib/Cargo.toml | sed -ne 's/^version[ ="]*\([0-9\.]*\).*$/\1/p')
 
 echo "old version: $old_tag"
-echo "new version: $new_tag"
+echo "version bin: $tag_bin"
+echo "version lib: $tag_lib"
+
+if [ $tag_bin != $tag_lib ]; then
+	echo -e "${RED}The versions of lib ($tag_lib) and bin ($tag_bin) must be same!${END}"
+	exit 1
+fi
+
+new_tag=$tag_bin
 
 if [ $new_tag == $old_tag ]; then
 	echo -e "${RED}New version $new_tag already exists!${END}"
-	echo -e "${RED}ABORT${END}"
 	exit 1
 fi
 
--- helpers/unused_dependencies.sh
+++ helpers/unused_dependencies.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-echo -e "\033[1;30mtest bin\033[0m"
-cargo +nightly udeps --quiet --bin versatiles --all-features
-
-echo -e "\033[1;30mtest lib\033[0m"
-cargo +nightly udeps --quiet --lib
+echo -e "\033[1;30mfind unused dependencies033[0m"
+cargo +nightly udeps --quiet --workspace
