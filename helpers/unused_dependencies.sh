62bb38a916b75dee7c1dd17d3b1f2d1c2442b1c9
Michael Kreil <github@michael-kreil.de>
2023-09-25 15:50:49 +0200

    style: fix helper logging

--- helpers/unused_dependencies.sh
+++ helpers/unused_dependencies.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")/.."
 
-echo -e "\033[1;30mfind unused dependencies033[0m"
-cargo +nightly udeps --quiet --workspace
+echo -e "\033[1;30mfind unused dependencies\033[0m"
+cargo +nightly udeps --workspace
