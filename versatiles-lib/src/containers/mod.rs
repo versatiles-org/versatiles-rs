9ffef792dc01516d295def83bb69dc22e15bf2dd
Michael Kreil <github@michael-kreil.de>
2024-05-06 22:47:17 +0200

    refactor: move reader trait

--- versatiles-lib/src/containers/mod.rs
+++ versatiles-lib/src/containers/mod.rs
@@ -1,18 +1,24 @@
 #[cfg(feature = "full")]
 pub mod directory;
+
 #[cfg(feature = "full")]
 mod getters;
+
 #[cfg(feature = "full")]
 pub mod mbtiles;
+
 #[cfg(feature = "full")]
 pub mod tar;
 
 #[cfg(feature = "mock")]
 pub mod mock;
 
-mod traits;
 pub mod versatiles;
+
+mod readers;
+mod traits;
 
 #[cfg(feature = "full")]
 pub use getters::*;
+pub use readers::*;
 pub use traits::*;
