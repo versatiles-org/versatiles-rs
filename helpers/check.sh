deed44b0a95353f2560d06689a810ddd7bdfd106
Michael Kreil <github@michael-kreil.de>
2024-05-07 12:50:03 +0200

    ci: thorough testing

--- helpers/check.sh
+++ helpers/check.sh
@@ -1,22 +1,29 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")/.."
 
-echo "check cargo fmt"
+echo "cargo fmt"
 result=$(cargo fmt -- --check 2>&1)
 if [ $? -ne 0 ]; then
    echo -e "$result\nERROR DURING: cargo fmt"
    exit 1
 fi
 
-echo "check cargo clippy "
+echo "cargo clippy "
 result=$(cargo clippy --workspace --all-features -- -D warnings 2>&1)
 if [ $? -ne 0 ]; then
    echo -e "$result\nERROR DURING: cargo clippy"
    exit 1
 fi
 
-echo "check cargo test"
-result=$(cargo test --workspace --all-features 2>&1)
+echo "cargo test library"
+result=$(cargo test -p versatiles-lib --all-features 2>&1)
+if [ $? -ne 0 ]; then
+   echo -e "$result\nERROR DURING: cargo test bin"
+   exit 1
+fi
+
+echo "cargo test binary"
+result=$(cargo test -p versatiles --all-features 2>&1)
 if [ $? -ne 0 ]; then
    echo -e "$result\nERROR DURING: cargo test bin"
    exit 1
