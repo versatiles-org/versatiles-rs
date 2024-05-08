a327b106c0f2a27678ab379b30d7526510afd81d
Michael Kreil <github@michael-kreil.de>
2024-05-08 23:04:18 +0200

    ci: check unused dependencies

--- helpers/unused_dependencies.sh
+++ helpers/unused_dependencies.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")/.."
 
-echo -e "\033[1;30mfind unused dependencies\033[0m"
-cargo +nightly udeps --workspace --all-targets
+echo -e "\033[1;33munused dependencies for binary\033[0m"
+cargo +nightly udeps -q --bins
+
+echo -e "\033[1;33munused dependencies for library (minimal)\033[0m"
+cargo +nightly udeps -q --lib --no-default-features
+
+echo -e "\033[1;33munused dependencies for library (http)\033[0m"
+cargo +nightly udeps -q --lib --no-default-features --features http
+
+echo -e "\033[1;33munused dependencies for library (full)\033[0m"
+cargo +nightly udeps -q --lib --no-default-features --features full
