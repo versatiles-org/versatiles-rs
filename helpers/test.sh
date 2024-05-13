1d8ac4d69331aab8a1dfed979ef832e6d89a4237
Michael Kreil <github@michael-kreil.de>
2024-05-13 18:07:40 +0200

    ci: less verbose

--- helpers/test.sh
+++ helpers/test.sh
@@ -3,14 +3,14 @@ cd "$(dirname "$0")/.."
 
 set -e
 
-cargo fmt --check
+cargo fmt
 
-cargo clippy --bins $1
-cargo clippy --lib --no-default-features $1
-cargo clippy --lib --no-default-features -F http $1
-cargo clippy --lib --no-default-features -F full $1
+cargo clippy --quiet --bins $1
+cargo clippy --quiet --lib --no-default-features $1
+cargo clippy --quiet --lib --no-default-features -F http $1
+cargo clippy --quiet --lib --no-default-features -F full $1
 
-cargo test --bins $1
-cargo test --lib --no-default-features $1
-cargo test --lib --no-default-features -F http $1
-cargo test --lib --no-default-features -F full $1
+cargo test --quiet --bins $1
+cargo test --quiet --lib --no-default-features $1
+cargo test --quiet --lib --no-default-features -F http $1
+cargo test --quiet --lib --no-default-features -F full $1
