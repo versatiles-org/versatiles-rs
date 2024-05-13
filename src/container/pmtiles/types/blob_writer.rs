55ee588ccc966a49ca77e971e878cc9aaad94fb9
Michael Kreil <github@michael-kreil.de>
2024-05-13 19:04:42 +0200

    test: blob_writer

--- src/container/pmtiles/types/blob_writer.rs
+++ src/container/pmtiles/types/blob_writer.rs
@@ -37,3 +37,55 @@ impl BlobWriter {
 		Blob::from(self.cursor.into_inner())
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use byteorder::{LittleEndian, ReadBytesExt};
+	use std::io::Cursor;
+
+	#[test]
+	fn test_write_varint() -> Result<()> {
+		let mut writer = BlobWriter::new();
+		writer.write_varint(300)?;
+		assert_eq!(writer.into_blob().as_slice(), &[0xAC, 0x02]);
+		Ok(())
+	}
+
+	#[test]
+	fn test_write_u8() -> Result<()> {
+		let mut writer = BlobWriter::new();
+		writer.write_u8(0xFF)?;
+		assert_eq!(writer.into_blob().as_slice(), &[0xFF]);
+		Ok(())
+	}
+
+	#[test]
+	fn test_write_i32() -> Result<()> {
+		let mut writer = BlobWriter::new();
+		writer.write_i32(-1)?;
+		let blob = writer.into_blob();
+		let mut cursor = Cursor::new(blob.as_slice());
+		assert_eq!(cursor.read_i32::<LittleEndian>()?, -1);
+		Ok(())
+	}
+
+	#[test]
+	fn test_write_u64() -> Result<()> {
+		let mut writer = BlobWriter::new();
+		writer.write_u64(u64::MAX)?;
+		let blob = writer.into_blob();
+		let mut cursor = Cursor::new(blob.as_slice());
+		assert_eq!(cursor.read_u64::<LittleEndian>()?, u64::MAX);
+		Ok(())
+	}
+
+	#[test]
+	fn test_write_slice() -> Result<()> {
+		let mut writer = BlobWriter::new();
+		let data = [0xDE, 0xAD, 0xBE, 0xEF];
+		writer.write_slice(&data)?;
+		assert_eq!(writer.into_blob().as_slice(), &data);
+		Ok(())
+	}
+}
