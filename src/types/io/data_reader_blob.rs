5bae7ea416a322bd20f196f6d5ef515ea74b0383
Michael Kreil <github@michael-kreil.de>
2024-05-16 11:56:14 +0000

    test: data_reader_blob

--- src/types/io/data_reader_blob.rs
+++ src/types/io/data_reader_blob.rs
@@ -69,8 +69,8 @@ impl From<Vec<u8>> for DataReaderBlob {
 #[cfg(test)]
 mod tests {
 	use super::*;
+	use crate::types::io::types::DataWriterTrait;
 
-	// Test the 'from_blob' method
 	#[tokio::test]
 	async fn from_blob() -> Result<()> {
 		let blob = Blob::from(vec![0, 1, 2, 3, 4, 5, 6, 7]);
@@ -87,6 +87,92 @@ mod tests {
 		);
 
 		assert!(data_reader.read_range(&ByteRange::new(0, 9)).await.is_err());
+
+		Ok(())
+	}
+
+	#[tokio::test]
+	async fn from_vec() -> Result<()> {
+		let data = vec![10, 20, 30, 40, 50, 60, 70, 80];
+		let mut data_reader = DataReaderBlob::from(data.clone());
+
+		assert_eq!(data_reader.get_name(), "memory");
+		assert_eq!(data_reader.len(), data.len());
+
+		let range = ByteRange::new(2, 4);
+		let result = data_reader.read_range(&range).await?;
+		assert_eq!(result.as_slice(), &data[2..6]);
+
+		Ok(())
+	}
+
+	#[tokio::test]
+	async fn from_data_writer_blob() -> Result<()> {
+		let data = [100u8, 101, 102, 103, 104, 105].as_slice();
+		let mut data_writer = DataWriterBlob::new()?;
+		data_writer.append(&Blob::from(data))?;
+		let mut data_reader: DataReaderBlob = data_writer.into();
+
+		assert_eq!(data_reader.get_name(), "memory");
+		assert_eq!(data_reader.len(), 6);
+
+		let range = ByteRange::new(0, 6);
+		let result = data_reader.read_range(&range).await?;
+		assert_eq!(result.as_slice(), data);
+
+		Ok(())
+	}
+
+	#[tokio::test]
+	async fn from_boxed_data_writer_blob() -> Result<()> {
+		let data = [100u8, 101, 102, 103, 104, 105].as_slice();
+		let mut data_writer = DataWriterBlob::new()?;
+		data_writer.append(&Blob::from(data))?;
+		let mut data_reader: DataReaderBlob = Box::new(data_writer).into();
+
+		assert_eq!(data_reader.get_name(), "memory");
+		assert_eq!(data_reader.len(), 6);
+
+		let range = ByteRange::new(0, 6);
+		let result = data_reader.read_range(&range).await?;
+		assert_eq!(result.as_slice(), data);
+
+		Ok(())
+	}
+
+	#[tokio::test]
+	async fn test_len() {
+		let data = vec![1, 2, 3, 4, 5];
+		let data_reader = DataReaderBlob::from(data.clone());
+		assert_eq!(data_reader.len(), data.len());
+	}
+
+	#[tokio::test]
+	async fn test_read() {
+		let data = vec![1, 2, 3, 4, 5];
+		let mut data_reader = DataReaderBlob::from(data);
+
+		let mut buffer = [0; 3];
+		let n = data_reader.read(&mut buffer).unwrap();
+		assert_eq!(n, 3);
+		assert_eq!(buffer, [1, 2, 3]);
+
+		let n = data_reader.read(&mut buffer).unwrap();
+		assert_eq!(n, 2);
+		assert_eq!(&buffer[..2], &[4, 5]);
+	}
+
+	// Test edge cases for 'read_range' method
+	#[tokio::test]
+	async fn test_read_range_edge_cases() -> Result<()> {
+		let data = vec![10, 20, 30, 40, 50, 60, 70, 80];
+		let mut data_reader = DataReaderBlob::from(data);
+
+		// Range starts at the end of the data
+		assert!(data_reader.read_range(&ByteRange::new(8, 1)).await.is_err());
+
+		// Range starts within the data but exceeds its length
+		assert!(data_reader.read_range(&ByteRange::new(6, 3)).await.is_err());
 
 		Ok(())
 	}
