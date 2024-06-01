769195a525412f71842bccee35a34539af14e326
Michael Kreil <github@michael-kreil.de>
2024-06-01 17:46:46 +0000

    style: format derive

--- versatiles_derive/src/lib.rs
+++ versatiles_derive/src/lib.rs
@@ -22,34 +22,34 @@ pub fn yaml_parser_derive(input: TokenStream) -> TokenStream {
 	};
 
 	let field_parsing = fields.iter().map(|field| {
-        let field_name = &field.ident;
-        let field_type = &field.ty;
-           let field_str = field_name.as_ref().unwrap().to_string();
+		let field_name = &field.ident;
+		let field_type = &field.ty;
+		let field_str = field_name.as_ref().unwrap().to_string();
 
-        match quote!(#field_type).to_string().as_str() {
-            "String" => quote! {
-                #field_name: yaml.hash_get_string(#field_str).context(format!("Failed to get '{}'", #field_str))?
-            },
-            "bool" => quote! {
-                #field_name: yaml.hash_get_bool(#field_str).unwrap_or(false)
-            },
-            "Option < String >" => quote! {
-                #field_name: yaml.hash_get_string(#field_str).ok()
-            },
-            _ => quote! {
-                #field_name: yaml.hash_get_str(#field_str).context(format!("Failed to get '{}'", #field_str))?.parse::<#field_type>().context(format!("Failed to parse '{}'", #field_str))?
-            },
-        }
-    });
+		match quote!(#field_type).to_string().as_str() {
+			"String" => quote! {
+				#field_name: yaml.hash_get_string(#field_str).context(format!("Failed to get '{}'", #field_str))?
+			},
+			"bool" => quote! {
+				#field_name: yaml.hash_get_bool(#field_str).unwrap_or(false)
+			},
+			"Option < String >" => quote! {
+				#field_name: yaml.hash_get_string(#field_str).ok()
+			},
+			_ => quote! {
+				#field_name: yaml.hash_get_str(#field_str).context(format!("Failed to get '{}'", #field_str))?.parse::<#field_type>().context(format!("Failed to parse '{}'", #field_str))?
+			},
+		}
+	});
 
 	let expanded = quote! {
-		 impl #name {
-			  pub fn from_yaml(yaml: &YamlWrapper) -> anyhow::Result<Self> {
-					Ok(Self {
-						 #(#field_parsing),*
-					})
-			  }
-		 }
+		impl #name {
+			pub fn from_yaml(yaml: &YamlWrapper) -> anyhow::Result<Self> {
+				Ok(Self {
+					#(#field_parsing),*
+				})
+			}
+		}
 	};
 
 	TokenStream::from(expanded)
