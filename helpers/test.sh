780cc03af57f68a7fe398a3670afa6cde3365a90
Michael Kreil <github@michael-kreil.de>
2024-07-07 09:09:10 +0000

    build: improve testing and udeps script

--- helpers/test.sh
+++ helpers/test.sh
@@ -6,26 +6,23 @@ set -e
 echo -e "\033[1;33mFormatting...\033[0m"
 cargo fmt
 
-echo -e "\033[1;33mRunning clippy for binary (tiny)...\033[0m"
-cargo clippy --quiet --bin versatiles --no-default-features -F cli $1
-
-echo -e "\033[1;33mRunning clippy for binary (big)...\033[0m"
+echo -e "\033[1;33mRunning clippy for binary...\033[0m"
 cargo clippy --quiet --bin versatiles --all-features $1
 
 echo -e "\033[1;33mRunning clippy for library...\033[0m"
 cargo clippy --quiet --lib --no-default-features $1
 
-echo -e "\033[1;33mRunning clippy for library (full)...\033[0m"
+echo -e "\033[1;33mRunning clippy for library (big)...\033[0m"
 cargo clippy --quiet --lib --all-features $1
 
 echo -e "\033[1;33mRunning tests for binary...\033[0m"
-cargo test --quiet --bins $1
+cargo test --quiet --bins --all-features $1
 
 echo -e "\033[1;33mRunning tests for library...\033[0m"
 cargo test --quiet --lib --no-default-features $1
 
-echo -e "\033[1;33mRunning tests for library (full)...\033[0m"
+echo -e "\033[1;33mRunning tests for library (big)...\033[0m"
 cargo test --quiet --lib --all-features $1
 
-echo -e "\033[1;33mRunning doc tests (full)...\033[0m"
+echo -e "\033[1;33mRunning doc tests (big)...\033[0m"
 cargo test --quiet --doc --all-features $1
--- helpers/unused_dependencies.sh
+++ helpers/unused_dependencies.sh
@@ -7,14 +7,8 @@ cargo +nightly udeps -q --bins
 echo -e "\033[1;33munused dependencies for library (minimal)\033[0m"
 cargo +nightly udeps -q --lib --workspace --no-default-features
 
-echo -e "\033[1;33munused dependencies for versatiles_container (cli)\033[0m"
-cargo +nightly udeps -q -p versatiles_container --lib --no-default-features --features cli
+echo -e "\033[1;33munused dependencies for library (cli)\033[0m"
+cargo +nightly udeps -q -p versatiles_container -p versatiles_core --lib --no-default-features --features cli
 
-echo -e "\033[1;33munused dependencies for versatiles_container (test)\033[0m"
-cargo +nightly udeps -q -p versatiles_container --lib --no-default-features --features test
-
-echo -e "\033[1;33munused dependencies for versatiles_core (cli)\033[0m"
-cargo +nightly udeps -q -p versatiles_core --lib --no-default-features --features cli
-
-echo -e "\033[1;33munused dependencies for versatiles_core (test)\033[0m"
-cargo +nightly udeps -q -p versatiles_core --lib --no-default-features --features test
+echo -e "\033[1;33munused dependencies for library (test)\033[0m"
+cargo +nightly udeps -q -p versatiles_container -p versatiles_core --lib --no-default-features --features test
