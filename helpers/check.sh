ff4a72d59fa6c8ac5db9e91d06cac03b17a5bf2e
Michael Kreil <github@michael-kreil.de>
2025-01-23 15:04:13 +0100

    ci: run clippy in every sub package

--- helpers/check.sh
+++ helpers/check.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")/.."
 
+PROJECT_DIR=$(pwd)
+
 echo "cargo check"
 result=$(cargo check --workspace --all-features --all-targets 2>&1)
 if [ $? -ne 0 ]; then
@@ -15,19 +17,27 @@ if [ $? -ne 0 ]; then
    exit 1
 fi
 
-echo "cargo clippy "
-result=$(cargo clippy --workspace --all-features --all-targets -- -D warnings 2>&1)
-if [ $? -ne 0 ]; then
-   echo -e "$result\nERROR DURING: cargo clippy"
-   exit 1
-fi
+echo "cargo clippy:"
 
-#echo "cargo test library"
-#result=$(cargo test --lib --all-features 2>&1)
-#if [ $? -ne 0 ]; then
-#   echo -e "$result\nERROR DURING: cargo test bin"
-#   exit 1
-#fi
+run_clippy() {
+   cd "${PROJECT_DIR}$1"
+   echo "  - $1"
+   result=$(cargo clippy --all-features --all-targets -- -D warnings 2>&1)
+   if [ $? -ne 0 ]; then
+      echo -e "$result\nERROR DURING: cargo clippy $1"
+      exit 1
+   fi
+}
+
+run_clippy /
+run_clippy /versatiles
+run_clippy /versatiles_container
+run_clippy /versatiles_core
+run_clippy /versatiles_derive
+run_clippy /versatiles_geometry
+run_clippy /versatiles_image
+run_clippy /versatiles_pipeline
+cd $PROJECT_DIR
 
 echo "cargo test binary"
 result=$(cargo test --bins --all-features 2>&1)
