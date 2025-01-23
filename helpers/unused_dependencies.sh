db04d7a1d74b58f8533cfd9b9d8d57319fc0f568
Michael Kreil <github@michael-kreil.de>
2025-01-23 17:15:24 +0100

    build: check for unused dependencies

--- helpers/unused_dependencies.sh
+++ helpers/unused_dependencies.sh
@@ -8,7 +8,10 @@ echo -e "\033[1;33munused dependencies for library (minimal)\033[0m"
 cargo +nightly udeps -q --lib --workspace --no-default-features
 
 echo -e "\033[1;33munused dependencies for library (cli)\033[0m"
-cargo +nightly udeps -q -p versatiles_container -p versatiles_core --lib --no-default-features --features cli
+cargo +nightly udeps -q --lib --workspace --no-default-features --features cli --exclude versatiles
 
 echo -e "\033[1;33munused dependencies for library (test)\033[0m"
-cargo +nightly udeps -q -p versatiles_container -p versatiles_core --lib --no-default-features --features test
+cargo +nightly udeps -q --lib --workspace --no-default-features --features test
+
+echo -e "\033[1;33munused dependencies for library (all-features)\033[0m"
+cargo +nightly udeps -q --lib --workspace --all-features --exclude versatiles --exclude versatiles_core
