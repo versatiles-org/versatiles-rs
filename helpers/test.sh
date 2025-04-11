09e2d7827af21b44cd39159176f241de153a706d
Michael Kreil <github@michael-kreil.de>
2025-04-11 12:37:02 +0200

    build: increase clippy testing

--- helpers/test.sh
+++ helpers/test.sh
@@ -6,18 +6,21 @@ set -e
 # Set environment variable
 RUST_BACKTRACE=1
 
+# Format
 echo -e "\033[1;33mFormatting...\033[0m"
 cargo fmt
 
+# Clippy
 echo -e "\033[1;33mRunning clippy for binary...\033[0m"
-cargo clippy --quiet --bin versatiles --all-features $1
+cargo clippy --quiet --bin versatiles --all-features --all-targets $1
 
 echo -e "\033[1;33mRunning clippy for library...\033[0m"
-cargo clippy --quiet --lib --no-default-features $1
+cargo clippy --quiet --lib --no-default-features --all-targets $1
 
 echo -e "\033[1;33mRunning clippy for library (big)...\033[0m"
-cargo clippy --quiet --lib --all-features $1
+cargo clippy --quiet --lib --all-features --all-targets $1
 
+# Test
 echo -e "\033[1;33mRunning tests for binary...\033[0m"
 cargo test --quiet --bins --all-features $1
 
