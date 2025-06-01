38d873f43ddceb29fbd34eb206fa1f0c0a537f88
Michael Kreil <github@michael-kreil.de>
2025-06-01 19:25:07 +0200

    fix: clippy warnings

--- versatiles_core/benches/byte_iterator.rs
+++ versatiles_core/benches/byte_iterator.rs
@@ -1,5 +1,5 @@
-use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion};
-use std::io::Cursor;
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use std::{hint::black_box, io::Cursor};
 use versatiles_core::byte_iterator::ByteIterator;
 
 const DATA_SIZE: usize = 100 * 1024 * 1024;
