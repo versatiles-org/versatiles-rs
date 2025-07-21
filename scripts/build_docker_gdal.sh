4cf698691b29474fff6e6591f536472c97bb80a7
Michael Kreil <github@michael-kreil.de>
2025-07-21 13:20:38 +0200

    prepare docker for debian+gdal

---
+++ scripts/build_docker_gdal.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")/.."
+
+docker build -t versatiles-gdal docker/gdal-debian
+
+# docker run -it --rm -p 8080:8080 --volume .:/data versatiles-gdal serve -s frontend-dev.br.tar world.vpl
+
+# time docker run -it --rm --volume .:/data versatiles-gdal convert world.vpl temp.versatiles --max-zoom 3
+
+
+# docker run -it --rm --volume .:/data --entrypoint "/usr/bin/gdaladdo" versatiles-gdal world.overview.tif
+
