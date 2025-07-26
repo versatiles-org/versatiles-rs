762e7c47a67636a67bdd8570b98797ac720d5ab6
Michael Kreil <github@michael-kreil.de>
2025-07-26 19:49:30 +0200

    fix: remove output path from coverage script command

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 mkdir -p "$(dirname "$0")/../target/llvm-cov"
-cargo llvm-cov test --bins --all-features --tests --lcov --output-path lcov.info $1
+cargo llvm-cov test --bins --all-features --tests --lcov $1
 cargo llvm-cov report
