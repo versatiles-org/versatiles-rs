d02db558fc5fccf5d052a29d3a8cb78d4738e857
Michael Kreil <github@michael-kreil.de>
2025-07-26 20:54:40 +0200

    refactor: add tests for Args parsing functionality

--- versatiles_derive/src/args.rs
+++ versatiles_derive/src/args.rs
@@ -4,6 +4,7 @@ use proc_macro2::TokenStream as TokenStream2;
 use syn::Token;
 use syn::parse::{self, Parse, ParseStream};
 
+#[derive(Debug)]
 pub struct Args(pub Option<Token![move]>, pub TokenStream2);
 impl Parse for Args {
 	fn parse(input: ParseStream<'_>) -> parse::Result<Self> {
@@ -15,5 +16,39 @@ impl Parse for Args {
 			None
 		};
 		Ok(Self(move_token, input.parse()?))
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::Args;
+	use syn::parse_str;
+
+	#[test]
+	fn test_parse_without_move() {
+		let args: Args = parse_str("foo").expect("Failed to parse without move");
+		assert!(args.0.is_none());
+		assert_eq!(args.1.to_string(), "foo");
+	}
+
+	#[test]
+	fn test_parse_with_move() {
+		let args: Args = parse_str("move, foo").expect("Failed to parse with move");
+		assert!(args.0.is_some());
+		assert_eq!(args.1.to_string(), "foo");
+	}
+
+	#[test]
+	fn test_parse_complex_expression() {
+		let input = "move, a + b * c";
+		let args: Args = parse_str(input).expect("Failed to parse complex expression");
+		assert!(args.0.is_some());
+		assert_eq!(args.1.to_string(), "a + b * c");
+	}
+
+	#[test]
+	fn test_parse_invalid_missing_comma() {
+		let err = parse_str::<Args>("move foo").unwrap_err();
+		assert!(err.to_string().contains(","), "Expected comma error, got: {}", err);
 	}
 }
