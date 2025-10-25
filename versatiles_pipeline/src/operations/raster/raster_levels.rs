01419c1659acebf9e1d6d91b33daf24d73f77c47
Michael Kreil <github@michael-kreil.de>
2025-10-26 01:40:53 +0200

    test: raster_levels

--- versatiles_pipeline/src/operations/raster/raster_levels.rs
+++ versatiles_pipeline/src/operations/raster/raster_levels.rs
@@ -9,11 +9,11 @@ use versatiles_image::traits::*;
 #[derive(versatiles_derive::VPLDecode, Clone, Debug)]
 /// Adjust brightness, contrast and gamma of raster tiles.
 struct Args {
-	/// Brightness adjustment. Defaults to 0.0 (no change).
+	/// Brightness adjustment, between -255 and 255. Defaults to 0.0 (no change).
 	brightness: Option<f32>,
-	/// Contrast adjustment. Defaults to 1.0 (no change).
+	/// Contrast adjustment, between 0 and infinity. Defaults to 1.0 (no change).
 	contrast: Option<f32>,
-	/// Gamma adjustment. Defaults to 1.0 (no change).
+	/// Gamma adjustment, between 0 and infinity. Defaults to 1.0 (no change).
 	gamma: Option<f32>,
 }
 
@@ -105,32 +105,34 @@ mod tests {
 	use rstest::rstest;
 
 	#[rstest]
-	#[case::no_change("6", [0.0, 1.0, 1.0], &[102])]
-	#[case::no_change("67", [0.0, 1.0, 1.0], &[102, 119])]
-	#[case::no_change("678", [0.0, 1.0, 1.0], &[102, 119, 136])]
-	#[case::no_change("6789", [0.0, 1.0, 1.0], &[102, 119, 136, 153])]
-	#[case::alpha_does_not_change("6", [20.0, 1.1, 0.9], &[129])]
-	#[case::alpha_does_not_change("67", [20.0, 1.1, 0.9], &[129, 119])]
-	#[case::alpha_does_not_change("678", [20.0, 1.1, 0.9], &[129, 147, 165])]
-	#[case::alpha_does_not_change("6789", [20.0, 1.1, 0.9], &[129, 147, 165, 153])]
-	#[case::medium("37A", [0.0, 1.0, 1.0], &[51, 119, 170])]
-	#[case::brightness_dec("37A", [-100.0, 1.0, 1.0], &[0, 19, 70])]
-	#[case::brightness_inc("37A", [100.0, 1.0, 1.0], &[151, 219, 255])]
-	#[case::contrast_dec("37A", [0.0, 0.5, 1.0], &[89, 123, 149])]
-	#[case::contrast_inc("37A", [0.0, 2.0, 1.0], &[0, 111, 213])]
-	#[case::gamma_dec("37A", [0.0, 1.0, 0.5], &[114, 174, 208])]
-	#[case::gamma_inc("37A", [0.0, 1.0, 2.0], &[10, 56, 113])]
+	#[case::no_change("6", 0.0, 1.0, 1.0, &[102])]
+	#[case::no_change("67", 0.0, 1.0, 1.0, &[102, 119])]
+	#[case::no_change("678", 0.0, 1.0, 1.0, &[102, 119, 136])]
+	#[case::no_change("6789", 0.0, 1.0, 1.0, &[102, 119, 136, 153])]
+	#[case::alpha_does_not_change("6", 20.0, 1.1, 0.9, &[129])]
+	#[case::alpha_does_not_change("67", 20.0, 1.1, 0.9, &[129, 119])]
+	#[case::alpha_does_not_change("678", 20.0, 1.1, 0.9, &[129, 147, 165])]
+	#[case::alpha_does_not_change("6789", 20.0, 1.1, 0.9, &[129, 147, 165, 153])]
+	#[case::medium("37A", 0.0, 1.0, 1.0, &[51, 119, 170])]
+	#[case::brightness_dec("37A", -100.0, 1.0, 1.0, &[0, 19, 70])]
+	#[case::brightness_inc("37A", 100.0, 1.0, 1.0, &[151, 219, 255])]
+	#[case::contrast_dec("37A", 0.0, 0.5, 1.0, &[89, 123, 149])]
+	#[case::contrast_inc("37A", 0.0, 2.0, 1.0, &[0, 111, 213])]
+	#[case::gamma_dec("37A", 0.0, 1.0, 0.5, &[114, 174, 208])]
+	#[case::gamma_inc("37A", 0.0, 1.0, 2.0, &[10, 56, 113])]
 	#[tokio::test]
 	async fn color_change_test(
 		#[case] color_in: &str,
-		#[case] parameters: [f32; 3],
+		#[case] brightness: f32,
+		#[case] contrast: f32,
+		#[case] gamma: f32,
 		#[case] color_out: &[u8],
 	) -> Result<()> {
 		let op = Operation {
 			source: Box::new(DummyImageSource::new(&format!("{color_in}.png"), None, 4).unwrap()),
-			brightness: parameters[0],
-			contrast: parameters[1],
-			gamma: parameters[2],
+			brightness,
+			contrast,
+			gamma,
 		};
 		let mut tiles = op
 			.get_stream(TileBBox::from_min_and_max(8, 56, 56, 56, 56)?)
@@ -139,6 +141,32 @@ mod tests {
 			.await;
 		assert_eq!(tiles.len(), 1);
 		assert_eq!(tiles[0].1.as_image()?.average_color(), color_out);
+		Ok(())
+	}
+
+	#[rstest]
+	#[case(   0, 1.0, 1.0, [ 63, 157, 249])]
+	#[case(   0, 0.5, 1.0, [ 95, 142, 189])]
+	#[case(-127, 0.5, 1.0, [  0,  15,  62])]
+	#[case(-127, 0.5, 1.5, [  0,   4,  30])]
+	#[tokio::test]
+	async fn test_raster_levels(
+		#[case] brightness: i16,
+		#[case] contrast: f32,
+		#[case] gamma: f32,
+		#[case] expected_color: [u8; 3],
+	) -> Result<()> {
+		let factory = PipelineFactory::new_dummy();
+
+		let op = factory
+			.operation_from_vpl(&format!(
+				"from_debug format=png | raster_flatten color=[50,150,250] | raster_levels brightness={brightness} contrast={contrast} gamma={gamma}"
+			))
+			.await?;
+
+		let bbox = TileCoord::new(3, 2, 1)?.as_tile_bbox(1)?;
+		let adj = op.get_stream(bbox).await?.next().await.unwrap().1.into_image()?;
+		assert_eq!(adj.average_color(), expected_color);
 		Ok(())
 	}
 }
