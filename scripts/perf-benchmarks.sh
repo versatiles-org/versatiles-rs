4fe31678c8b68192bdd99c7422f92873237b385c
Michael Kreil <github@michael-kreil.de>
2025-10-25 15:47:39 +0200

    fix: update test command to include workspace option

--- scripts/perf-benchmarks.sh
+++ scripts/perf-benchmarks.sh
@@ -3,4 +3,4 @@ cd "$(dirname "$0")/.."
 
 # Run all tests and check their duration
 
-RUST_BACKTRACE=1 cargo +nightly test --bins --all-features -- -Zunstable-options --report-time
+RUST_BACKTRACE=1 cargo +nightly test --bins --lib --all-features --workspace -- -Zunstable-options --report-time
