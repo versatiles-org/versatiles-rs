9e6aba16fdec081fb04ef7413d017d6ad4430d1f
Michael Kreil <github@michael-kreil.de>
2025-10-26 21:07:05 +0100

    fix: ensure env-gdal.sh is sourced before running coverage tests

--- scripts/test-coverage.sh
+++ scripts/test-coverage.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 PROJECT_DIR="$(dirname "$0")/.."
+source "$PROJECT_DIR/scripts/env-gdal.sh"
 cargo llvm-cov test --bins --all-features --tests --lcov --output-path "$PROJECT_DIR/lcov.info" $1
 cargo llvm-cov report
