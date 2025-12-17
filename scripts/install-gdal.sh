937457f55c33f785e81d61dc6aac43b96f95d535
Michael Kreil <github@michael-kreil.de>
2025-12-17 17:10:37 +0100

    fix: revert GDAL version to 3.11.5

--- scripts/install-gdal.sh
+++ scripts/install-gdal.sh
@@ -21,7 +21,7 @@ set -euo pipefail
 : "${SKIP_DEPS:=0}"           # 1 = do not install deps
 : "${SRC_DIR:=/tmp/gdal-src}" # Working dir for downloading/building
 : "${INSTALL_TEST:=1}"        # 1 = run post-install sanity checks
-: "${GDAL_VERSION:=3.12.0}"   # GDAL version to install (compatible with gdal 0.18.0)
+: "${GDAL_VERSION:=3.11.5}"   # GDAL version to install (compatible with gdal 0.18.0)
 # =============================================================================
 
 # ----- tiny logger helpers ----------------------------------------------------
