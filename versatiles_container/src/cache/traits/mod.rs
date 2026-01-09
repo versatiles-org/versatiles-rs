93f35167eab2c51fab5e1fccfa7d90c890ff54ed
Michael Kreil <github@michael-kreil.de>
2026-01-09 14:05:35 +0100

    refactor: move CacheValue trait and implementations

--- versatiles_container/src/cache/traits/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Trait definitions for the VersaTiles caching subsystem.
-//!
-//! This module provides the [`CacheValue`] trait for serializing and
-//! deserializing values to/from the disk cache backend.
-
-mod value;
-
-pub use value::*;
