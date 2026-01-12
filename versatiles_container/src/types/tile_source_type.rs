071debdadba7bb0d35569ffe353a420f8a92d8ef
Michael Kreil <github@michael-kreil.de>
2026-01-12 14:33:15 +0100

    test: add unit tests for SourceType methods and display implementations

--- versatiles_container/src/types/tile_source_type.rs
+++ versatiles_container/src/types/tile_source_type.rs
@@ -68,3 +68,109 @@ impl Debug for SourceType {
 		}
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_new_container() {
+		let source = SourceType::new_container("mbtiles", "/path/to/file.mbtiles");
+		match source.as_ref() {
+			SourceType::Container { name, input } => {
+				assert_eq!(name, "mbtiles");
+				assert_eq!(input, "/path/to/file.mbtiles");
+			}
+			_ => panic!("Expected Container variant"),
+		}
+	}
+
+	#[test]
+	fn test_new_processor() {
+		let container = SourceType::new_container("mbtiles", "/path/to/file.mbtiles");
+		let processor = SourceType::new_processor("filter", container);
+		match processor.as_ref() {
+			SourceType::Processor { name, input } => {
+				assert_eq!(name, "filter");
+				match input.as_ref() {
+					SourceType::Container { name, .. } => assert_eq!(name, "mbtiles"),
+					_ => panic!("Expected Container as input"),
+				}
+			}
+			_ => panic!("Expected Processor variant"),
+		}
+	}
+
+	#[test]
+	fn test_new_composite() {
+		let source1 = SourceType::new_container("mbtiles", "/path/to/file1.mbtiles");
+		let source2 = SourceType::new_container("pmtiles", "/path/to/file2.pmtiles");
+		let composite = SourceType::new_composite("stacked", &[source1, source2]);
+		match composite.as_ref() {
+			SourceType::Composite { name, inputs } => {
+				assert_eq!(name, "stacked");
+				assert_eq!(inputs.len(), 2);
+			}
+			_ => panic!("Expected Composite variant"),
+		}
+	}
+
+	#[test]
+	fn test_display_container() {
+		let source = SourceType::new_container("mbtiles", "/path/to/file.mbtiles");
+		assert_eq!(format!("{}", source), "container 'mbtiles' ('/path/to/file.mbtiles')");
+	}
+
+	#[test]
+	fn test_display_processor() {
+		let container = SourceType::new_container("mbtiles", "/path/to/file.mbtiles");
+		let processor = SourceType::new_processor("filter", container);
+		assert_eq!(format!("{}", processor), "processor 'filter'");
+	}
+
+	#[test]
+	fn test_display_composite() {
+		let source1 = SourceType::new_container("mbtiles", "/path/to/file1.mbtiles");
+		let source2 = SourceType::new_container("pmtiles", "/path/to/file2.pmtiles");
+		let composite = SourceType::new_composite("stacked", &[source1, source2]);
+		assert_eq!(format!("{}", composite), "composite 'stacked'");
+	}
+
+	#[test]
+	fn test_debug_container() {
+		let source = SourceType::new_container("mbtiles", "/path/to/file.mbtiles");
+		let debug_str = format!("{:?}", source);
+		assert!(debug_str.contains("Container"));
+		assert!(debug_str.contains("mbtiles"));
+		assert!(debug_str.contains("/path/to/file.mbtiles"));
+	}
+
+	#[test]
+	fn test_debug_processor() {
+		let container = SourceType::new_container("mbtiles", "/path/to/file.mbtiles");
+		let processor = SourceType::new_processor("filter", container);
+		let debug_str = format!("{:?}", processor);
+		assert!(debug_str.contains("Processor"));
+		assert!(debug_str.contains("filter"));
+	}
+
+	#[test]
+	fn test_debug_composite() {
+		let source1 = SourceType::new_container("mbtiles", "/path/to/file1.mbtiles");
+		let source2 = SourceType::new_container("pmtiles", "/path/to/file2.pmtiles");
+		let composite = SourceType::new_composite("stacked", &[source1, source2]);
+		let debug_str = format!("{:?}", composite);
+		assert!(debug_str.contains("Composite"));
+		assert!(debug_str.contains("stacked"));
+	}
+
+	#[test]
+	fn test_clone_and_eq() {
+		let source1 = SourceType::new_container("mbtiles", "/path/to/file.mbtiles");
+		let source2 = source1.clone();
+		assert_eq!(source1, source2);
+
+		let source3 = SourceType::new_container("mbtiles", "/path/to/other.mbtiles");
+		assert_ne!(source1, source3);
+	}
+}
