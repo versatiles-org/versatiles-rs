c5cd88a8a8b96daf8b6e898e288f38530b0e290a
Michael Kreil <github@michael-kreil.de>
2026-01-16 18:25:57 +0100

    refactor: clarify comment on skipping e2e tests during coverage

--- scripts/test-coverage.sh
+++ scripts/test-coverage.sh
@@ -2,6 +2,6 @@
 
 PROJECT_DIR="$(dirname "$0")/.."
 source "$PROJECT_DIR/scripts/env-gdal.sh"
-# Skip e2e tests (files prefixed with e2e_) during coverage
+# Skip e2e tests (test functions prefixed with e2e_) during coverage
 cargo llvm-cov test --bins --all-features --tests --lcov --output-path "$PROJECT_DIR/lcov.info" $1 -- --skip e2e_
 cargo llvm-cov report
