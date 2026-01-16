bc40e86fb224ac548ec76aa7f46f85de64a02aab
Michael Kreil <github@michael-kreil.de>
2026-01-16 18:27:19 +0100

    refactor: update test coverage command to use workspace instead of bins

--- scripts/test-coverage.sh
+++ scripts/test-coverage.sh
@@ -3,5 +3,5 @@
 PROJECT_DIR="$(dirname "$0")/.."
 source "$PROJECT_DIR/scripts/env-gdal.sh"
 # Skip e2e tests (test functions prefixed with e2e_) during coverage
-cargo llvm-cov test --bins --all-features --tests --lcov --output-path "$PROJECT_DIR/lcov.info" $1 -- --skip e2e_
+cargo llvm-cov test --workspace --all-features --tests --lcov --output-path "$PROJECT_DIR/lcov.info" $1 -- --skip e2e_
 cargo llvm-cov report
