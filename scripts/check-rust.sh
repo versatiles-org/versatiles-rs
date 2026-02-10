4fdf5e473c7a5610b807b11d52a420d486afcaad
Michael Kreil <github@michael-kreil.de>
2026-02-10 22:22:42 +0100

    fix: add color output to cargo commands in check-rust.sh for better visibility

--- scripts/check-rust.sh
+++ scripts/check-rust.sh
@@ -12,38 +12,38 @@ echo "Rust Checks"
 echo "=========================================="
 
 echo "cargo fmt"
-result=$(cargo fmt -- --check 2>&1)
+result=$(cargo fmt -- --check --color=always 2>&1)
 if [ $? -ne 0 ]; then
    echo -e "$result\nERROR DURING: cargo fmt"
    exit 1
 fi
 
 echo "cargo check"
-result=$(cargo check --workspace --no-default-features --all-targets 2>&1)
+result=$(cargo check --color=always --workspace --no-default-features --all-targets 2>&1)
 if [ $? -ne 0 ]; then
    echo -e "$result\nERROR DURING: cargo check"
    exit 1
 fi
 echo "cargo check - server"
-result=$(cargo check --workspace --no-default-features --features server --all-targets 2>&1)
+result=$(cargo check --color=always --workspace --no-default-features --features server --all-targets 2>&1)
 if [ $? -ne 0 ]; then
    echo -e "$result\nERROR DURING: cargo check"
    exit 1
 fi
 echo "cargo check - cli"
-result=$(cargo check --workspace --no-default-features --features cli --all-targets 2>&1)
+result=$(cargo check --color=always --workspace --no-default-features --features cli --all-targets 2>&1)
 if [ $? -ne 0 ]; then
    echo -e "$result\nERROR DURING: cargo check"
    exit 1
 fi
 echo "cargo check - server, cli"
-result=$(cargo check --workspace --no-default-features --features server,cli --all-targets 2>&1)
+result=$(cargo check --color=always --workspace --no-default-features --features server,cli --all-targets 2>&1)
 if [ $? -ne 0 ]; then
    echo -e "$result\nERROR DURING: cargo check"
    exit 1
 fi
 echo "cargo check - all features"
-result=$(cargo check --workspace --all-features --all-targets 2>&1)
+result=$(cargo check --color=always --workspace --all-features --all-targets 2>&1)
 if [ $? -ne 0 ]; then
    echo -e "$result\nERROR DURING: cargo check"
    exit 1
@@ -51,7 +51,7 @@ fi
 
 echo "cargo clippy"
 cd $PROJECT_DIR
-result=$(cargo clippy --workspace --all-features --all-targets -- -D warnings 2>&1)
+result=$(cargo clippy --color=always --workspace --all-features --all-targets -- -D warnings 2>&1)
 if [ $? -ne 0 ]; then
    echo -e "$result\nERROR DURING: cargo clippy"
    exit 1
@@ -59,7 +59,7 @@ fi
 
 # echo "cargo test"
 # cd $PROJECT_DIR
-# result=$(cargo test --no-fail-fast 2>&1)
+# result=$(cargo test --color=always 2>&1)
 # if [ $? -ne 0 ]; then
 #    echo -e "$result\nERROR DURING: cargo test"
 #    exit 1
@@ -67,7 +67,7 @@ fi
 
 echo "cargo test all features"
 cd $PROJECT_DIR
-result=$(cargo test --no-fail-fast --all-features 2>&1)
+result=$(cargo test --color=always --all-features 2>&1)
 if [ $? -ne 0 ]; then
    echo -e "$result\nERROR DURING: cargo test all features"
    exit 1
@@ -75,7 +75,7 @@ fi
 
 echo "cargo doc"
 cd $PROJECT_DIR
-result=$(RUSTDOCFLAGS="-D warnings" cargo doc --no-deps 2>&1)
+result=$(RUSTDOCFLAGS="-D warnings" cargo doc --color=always --no-deps 2>&1)
 if [ $? -ne 0 ]; then
    echo -e "$result\nERROR DURING: cargo doc"
    exit 1
