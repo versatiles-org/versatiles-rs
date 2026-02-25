cea97de9634214ff76bbb8b3bebc1205edb1b92a
Michael Kreil <github@michael-kreil.de>
2026-02-25 17:27:48 +0100

    fix(scripts): add --yes flag to markdownlint command

--- scripts/check-markdown.sh
+++ scripts/check-markdown.sh
@@ -8,7 +8,7 @@ echo "Markdown Checks"
 echo "=========================================="
 
 echo "markdownlint"
-result=$(npx markdownlint-cli2 "**/*.md" "#node_modules" "#versatiles_node/node_modules" "#target" 2>&1)
+result=$(npx --yes markdownlint-cli2 "**/*.md" "#node_modules" "#versatiles_node/node_modules" "#target" 2>&1)
 if [ $? -ne 0 ]; then
    echo -e "$result\nERROR DURING: markdownlint"
    exit 1
