0bf93c0cb9a488d7cb24825037d05005f74cd5fb
Michael Kreil <github@michael-kreil.de>
2026-04-13 01:16:07 +0900

    fix: update dimensions method to use getter for width and height

--- versatiles_core/src/types/tile_bbox/constructors.rs
+++ versatiles_core/src/types/tile_bbox/constructors.rs
@@ -350,7 +350,7 @@ impl TileBBox {
 
 	#[must_use]
 	pub fn dimensions(&self) -> (u32, u32) {
-		(self.width, self.height)
+		(self.width(), self.height())
 	}
 
 	/// Minimum x‑tile (column) coordinate.
